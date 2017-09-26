819f577ff7bbdce6a04bae7937f355253a423c0d
paulj <jaffrepaul@gmail.com>
2017-09-26 17:18:02 -0400

    added form to MeetingForm component

--- client/src/MeetingForm.jsx
+++ client/src/MeetingForm.jsx
@@ -14,7 +14,28 @@ class MeetingForm extends React.Component {
   render() {
     return (
       <div>
-      MEETINGFORM COMPONENT
+         <form action="/meetings" method="post">
+            <div>
+                <label>Meeting Name:</label>
+                <input type="text" id="meetingname" name="meeting_name"/>
+            </div>
+            <div>
+                <label>Meeting Duration:</label>
+                <input type="text" id="meetingduration" name="meeting_duration"/>
+            </div>
+            <div>
+                <label>Meeting Attendees:</label>
+                <input type="text" id="meetingattendees" name="meeting_attendees"/>
+            </div>
+            <div>
+                <label>Average Attendee Salary:</label>
+                <input type="text" id="attendeesalary" name="meeting_salary"/>
+            </div>
+
+            <div className="button">
+              <button type="submit">Get Meeting Cost</button>
+            </div>
+        </form>
       </div>
     );
   }

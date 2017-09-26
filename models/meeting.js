2accf43985d3127d1b4bbbcfab5b59c7b1a1a8c5
paulj <jaffrepaul@gmail.com>
2017-09-26 10:20:21 -0400

    GET & POST reqs working. need to verify POST validation errors

--- models/meeting.js
+++ models/meeting.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Meeting = new Schema({
+const MeetingSchema = new Schema({
   meetingName: {
     type: String,
     required: true
-  }
+  },
   meetingDuration: {
-    type: Number,
+    type: String,
     required: true
-  }
+  },
   meetingAttendees: {
     type: Number,
     required: true
-  }
+  },
   meetingSalary: {
-    type: Number,
+    type: String,
     required: true
   }
-
 });
 
-const Meeting = mongoose.model('Meeting', meetingSchema);
+const Meeting = mongoose.model('Meeting', MeetingSchema);
 
 module.exports = Meeting;
--- routes.js
+++ routes.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const Meeting = require('./models/meeting');
 
 // Route Handlers
 
 // get all entries from DB
 router.get('/meetings', function(req, res) {
-  res.send( 'GET req received' );
+  Meeting.find({})
+    .then(function(data) {
+      res.send(data);
+    }).catch(function(err) {
+      console.log('error getting data');
+    });
+  // res.send( 'GET req received' );
 });
 
 // add new entry to DB
 router.post('/meetings', function(req, res) {
-  res.send( 'POST req received' );
+  // let meetingName = req.body.name;
+  // let meetingDuration = req.body.duration;
+  // let meetingAttendees = req.body.attendees;
+  // let meetingSalary = req.body.salary;
+  //
+  // Meeting.create(
+  //   { meetingName: meetingName,
+  //     meetingDuration: meetingDuration,
+  //     meetingAttendees: meetingAttendees,
+  //     meetingSalary: meetingSalary
+  //   }, function(err, Meeting) {
+  //     if (err) {
+  //       console.log(err);
+  //     }
+  //     res.send(Meeting);
+  //   })
+
+    Meeting.create(req.body, function (err, Meeting) {
+    if (err) return console.log(err);
+    return res.send(Meeting);
+  });
 });
 
 // update entry in DB
